4f449ac1ee6b9502b66d43dfc02235a1fe8c1d16
Enée_Frédéric <46350788+Kikizork@users.noreply.github.com>
2019-11-21 15:40:19 +0100

    Delete SousGroupeIngredientRepository.java

--- IPI-LCB-LCB/src/main/java/App/LCB/repository/SousGroupeIngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package App.LCB.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import App.LCB.model.SousGroupeIngredient;
-
-public interface SousGroupeIngredientRepository extends CrudRepository<SousGroupeIngredient, Integer>{
-
-}
