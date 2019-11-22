04bb411685d4ce235950f395eecc6bc52638115f
jorisbonisseurdelabath <57140659+jorisbonisseurdelabath@users.noreply.github.com>
2019-11-22 14:55:51 +0100

    Update LesCordonsBleusmysql3.sql

--- DB LCB/LesCordonsBleusmysql3.sql
+++ DB LCB/LesCordonsBleusmysql3.sql
@@ -2,6 +2,6 @@ alter table recette add column nbr_personne SMALLINT(4);
 
 alter table recette add column url_recette VARCHAR(512);
 
-Update recette set nbr_personnes=2 where id_recette=1;
-Update recette set nbr_personnes=1 where id_recette=2;
-Update recette set nbr_personnes=4 where id_recette=3;
+Update recette set nbr_personne=2 where id_recette=1;
+Update recette set nbr_personne=1 where id_recette=2;
+Update recette set nbr_personne=4 where id_recette=3;
