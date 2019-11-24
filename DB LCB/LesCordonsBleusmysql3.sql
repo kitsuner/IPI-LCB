28fb2c5a0451456626c9e3de5119c237bbd779ca
Enée_Frédéric <46350788+Kikizork@users.noreply.github.com>
2019-11-24 20:41:51 +0100

    Update LesCordonsBleusmysql3.sql

--- DB LCB/LesCordonsBleusmysql3.sql
+++ DB LCB/LesCordonsBleusmysql3.sql
@@ -5,3 +5,4 @@ alter table recette add column url_recette VARCHAR(512);
 Update recette set nbr_personne=2 where id_recette=1;
 Update recette set nbr_personne=1 where id_recette=2;
 Update recette set nbr_personne=4 where id_recette=3;
+alter table recette add column image blob;
