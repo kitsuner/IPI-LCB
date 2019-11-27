45cd7d94925b68fa009a4aad096bee23d123fc25
jorisbonisseurdelabath <57140659+jorisbonisseurdelabath@users.noreply.github.com>
2019-11-27 14:13:46 +0100

    Create ListeFavorisController.java

---
+++ src/main/java/App/LCB/Controller/ListeFavorisController.java
@@ -0,0 +1,10 @@
+package App.LCB.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/liste_favoris")
+public class ListeFavorisController {
+
+}
